c1038ab776e02372fa8511d299d284707b67646e
Tiago Vieira <tiagopereiravieira@gmail.com>
2017-11-27 22:00:17 -0300

    upload release 5 27-11-2017

--- app/src/main/java/com/codigosandroid/androidutils/MainActivity.java
+++ app/src/main/java/com/codigosandroid/androidutils/MainActivity.java
@@ -1,8 +1,11 @@
 package com.codigosandroid.androidutils;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.codigosandroid.utils.activity.BaseActivity;
+import com.codigosandroid.utils.utils.AlertUtil;
+import com.codigosandroid.utils.utils.AndroidUtil;
 
 public class MainActivity extends BaseActivity {
 
